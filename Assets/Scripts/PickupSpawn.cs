ea6aefb8c89726bb02339863f9279fbbc851f6f4
Valeria Rincon <v.rincon558@gmail.com>
2021-10-20 13:04:15 -0400

    Ch. 10 - Added pickup spawn points

---
+++ Assets/Scripts/PickupSpawn.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] pickups;
+
+    void spawnPickup()
+    {
+        GameObject pickup = Instantiate(pickups[Random.Range(0, pickups.Length)]);
+        pickup.transform.position = transform.position;
+        pickup.transform.parent = transform;
+    }
+
+    IEnumerator respawnPickup()
+    {
+        yield return new WaitForSeconds(20);
+        spawnPickup();
+    }
+
+    void Start()
+    {
+        spawnPickup();
+    }
+
+    public void PickupWasPickedUp()
+    {
+        StartCoroutine("respawnPickup");
+    }
+}
