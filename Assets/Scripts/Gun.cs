70962ea197a971a898c9e23a2abd8dbfe6f3f941
Valeria Rincon <v.rincon558@gmail.com>
2021-10-20 11:39:21 -0400

    Ch. 10 - Added damage effects to Player and Robots

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -8,18 +8,45 @@ public class Gun : MonoBehaviour
     public Ammo ammo;
     public AudioClip liveFire;
     public AudioClip dryFire;
+    public float zoomFactor;
+    public int range;
+    public int damage;
 
     protected float lastFireTime;
+
+    private float zoomFOV;
+    private float zoomSpeed = 6;
 
     // Start is called before the first frame update
     void Start()
     {
+        zoomFOV = Constants.CameraDefaultZoom / zoomFactor;
         lastFireTime = Time.time - 10;
     }
 
     protected virtual void Update()
     {
+        if (Input.GetMouseButton(1))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, zoomFOV, zoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Constants.CameraDefaultZoom;
+        }
+    }
 
+    private void processHit(GameObject hitObject)
+    {
+        if (hitObject.GetComponent<Player>() != null)
+        {
+            hitObject.GetComponent<Player>().TakeDamage(damage);
+        }
+
+        if (hitObject.GetComponent<Robot>() != null)
+        {
+            hitObject.GetComponent<Robot>().TakeDamage(damage);
+        }
     }
 
     protected void Fire()
@@ -33,6 +60,13 @@ public class Gun : MonoBehaviour
         else
         {
             GetComponent<AudioSource>().PlayOneShot(dryFire);
+        }
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, range))
+        {
+            processHit(hit.collider.gameObject);
         }
     }
 }
