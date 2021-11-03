37f49a4f2697db493c69aa2f03a51cb04f064de6
Valeria Rincon <v.rincon558@gmail.com>
2021-11-03 11:42:55 -0400

    Ch. 11 - Added UI score text updates in Game script

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -19,6 +19,12 @@ public class Game : MonoBehaviour
     void Start()
     {
         singleton = this;
+        StartCoroutine("increaseScoreEachSecond");
+        isGameOver = false;
+        Time.timeScale = 1;
+        waveCountdown = 30;
+        enemiesLeft = 0;
+        StartCoroutine("updateWaveTimer");
         SpawnRobots();
     }
 
@@ -28,6 +34,52 @@ public class Game : MonoBehaviour
         {
             spawn.SpawnRobot();
             enemiesLeft++;
+        }
+        gameUI.SetEnemyText(enemiesLeft);
+    }
+
+    private IEnumerator updateWaveTimer()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1f);
+            waveCountdown--;
+            gameUI.SetWaveText(waveCountdown);
+
+            if (waveCountdown == 0)
+            {
+                SpawnRobots();
+                waveCountdown = 30;
+                gameUI.ShowNewWaveText();
+            }
+        }
+    }
+
+    public static void RemoveEnemy()
+    {
+        singleton.enemiesLeft--;
+        singleton.gameUI.SetEnemyText(singleton.enemiesLeft);
+
+        if (singleton.enemiesLeft == 0)
+        {
+            singleton.score += 50;
+            singleton.gameUI.ShowWaveClearBonus();
+        }
+    }
+
+    public void AddRobotKillToScore()
+    {
+        score += 10;
+        gameUI.SetScoreText(score);
+    }
+
+    IEnumerator increaseScoreEachSecond()
+    {
+        while (!isGameOver)
+        {
+            yield return new WaitForSeconds(1);
+            score += 1;
+            gameUI.SetScoreText(score);
         }
     }
 
